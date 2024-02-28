ddfa4ab815b84c4f05ece7661bf240acb09214a6
Chantal <chantal@Chantals-MacBook-Air.local>
2024-02-28 11:07:51 -0500

    dots scrolling added

--- src/scenes/DotGroup.jsx
+++ src/scenes/DotGroup.jsx
@@ -1,7 +1,51 @@
+import { useEffect, useState } from 'react';
 import AnchorLink from 'react-anchor-link-smooth-scroll'
 
-export default function DotGroup ({ selectedPage, setSelectedPage }) {
+export default function DotGroup({ selectedPage, setSelectedPage }) {
   const selectedStyle = 'relative bg-fucsia before:absolute before:w-6 before:h-6 before:rounded-full before:border-2 before:border-fucsia before:left-[-50%] before:top-[-50%]'
+
+
+
+  //////////////
+
+  useEffect(() => {
+    const handleScroll = () => {
+      // to obtain the vertical position:
+      const homeSection = document.getElementById('home').offsetTop;
+      const skillsSection = document.getElementById('skills').offsetTop;
+      const projectsSection = document.getElementById('projects').offsetTop;
+      const contactSection = document.getElementById('contact').offsetTop;
+
+      // it determines the actual scroll position 
+      const scrollPosition = window.scrollY;
+
+      //it compares the scroll position with the sections position 
+      if (scrollPosition >= homeSection && scrollPosition < skillsSection) {
+        setSelectedPage('home');
+      } else if (scrollPosition >= skillsSection && scrollPosition < projectsSection) {
+        setSelectedPage('skills');
+      } else if (scrollPosition >= projectsSection && scrollPosition < contactSection) {
+        setSelectedPage('projects');
+      } else if (scrollPosition >= contactSection) {
+        setSelectedPage('contact');
+      }
+    };
+
+    //when the component mounts - it adds the event listener 
+    window.addEventListener('scroll', handleScroll);
+
+    //it removes the event listener when component unmount 
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [setSelectedPage]);
+
+  /////////////////
+
+
+
+
+
 
   return (
     <div className='flex flex-col gap-6 fixed top-[60%] right-7'>
